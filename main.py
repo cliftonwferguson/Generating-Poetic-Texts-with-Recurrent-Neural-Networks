388ed7abb6f044e8889bac70f322d03c518ad1ac
Clifton Ferguson <clifton.ferguson7@gmail.com>
2023-07-08 16:44:20 -0500

    built char and index flow

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import random
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.layers import LSTM, Dense, Activation
+from tensorflow.python.keras.optimizers import RMSprop
 
+filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+text = open(filepath, 'rb').read().decode(encoding='utf-8').lower() 
+
+text = text[300000:800000]
+
+characters = sorted(set(text))
+
+char_to_index = dict((c, i)for i, c in enumerate(characters))
+index_to_char = dict((i, c)for i, c in enumerate(characters))
+
+sentences = []
+next_characters = []
